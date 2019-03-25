803b095bc7643e1b41df3b645552e9409ff7e735
ingJcbDev <ing.jcb.dev@gmail.com>
2019-03-25 15:33:21 -0500

    hasta ahora se ve como va se la situacion de los juego

---
+++ new/index.php
@@ -0,0 +1,34 @@
+<!-- http://www.johnkyrk.com/photosynthesis.esp.html -->
+<!-- http://flash.astalaweb.net/ -->
+<!-- https://sites.google.com/site/bancoderecursosmultimediales/animaciones -->
+
+<!-- Este es la cabecera de pagina -->
+<?php
+require_once "vistas/structure/header.php";
+?>
+
+<!-- Esta es las imagenes en movimiento -->
+<?php
+require_once "vistas/structure/slaider.php";
+?>
+
+
+<body>
+    <h1>
+        <center>Bootstrap 4 Body</center>
+    </h1>
+    <!-- <div>
+        <p><embed src="J-Letras.swf" width="1000" height="618"></p>
+    </div> -->
+
+<?php
+require_once "vistas/juegos/j_gallina.php";
+?>
+
+</body>
+
+
+<!-- Este es el pie de pagina  -->
+<?php
+require_once "vistas/structure/footer.php";
+?>
---
+++ new/vistas/structure/header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" href="assets/images/biologia/png/017-chlorophyll.png">
+    <title>La Biologia</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- css -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+
+</head>
+<header>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+        <!-- Image and text -->
+        <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php">
+                <img src="assets/images/biologia/png/021-laboratory.png" width="45" height="45"
+                    class="d-inline-block align-top" alt="">
+                Biologia
+            </a>
+        </nav>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
+                </li>
+                <!-- 
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                 -->
+                <!-- 
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                 -->
+                <!-- 
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+                 -->
+                <!-- 
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        Dropdown link
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li> -->
+
+            </ul>
+
+        </div>
+
+        <!-- Image and text -->
+        <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php">
+                <img src="assets/images/logocol.png" width="50" height="50" class="d-inline-block align-top" alt="">
+            </a>
+        </nav>
+
+    </nav>
+</header>
